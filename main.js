c434b2fc93d7e64174a6229f3c2440acad9d1966
Suman Das <sum4ndas@gmail.com>
2022-01-30 17:58:20 +0530

    Keyboard Support: Enter, backspace, operators.

--- main.js
+++ main.js
@@ -80,64 +80,65 @@ numButtons.forEach((button) => {
     });
 });
 
+const operateButtonFunc = (button) => {
+    // If input is only '.', operate buttons wont work till
+    // valid input.
+    if (num1 == '.') {
+        return;
+    }
+    dotButton.disabled = false;
+
+    display1.textContent += " " + button.textContent + " ";
+
+    // This check needs to be above the switch statement
+    // because we will operate on previous operator.
+    // Each operator presses will operate on previous numbers
+    // with previous operator.
+
+    // If operators are pressed after "=" newCalc will be false.
+    if (operator && newCalc) {
+        // num1 = operate(operator, Number(num1), Number(num2));
+        display2.textContent = num1;
+        newCalc = false;
+    } else if (operator && !newCalc) {
+        num1 = operate(operator, Number(num1), Number(num2));
+        if (num1 == Infinity || num1 == -Infinity) {
+            display2.textContent = "ERROR";
+        } else {
+            display2.textContent = num1;
+        } 
+    }
+            
+    switch (button.textContent) {
+        case '+':
+            operator = add;
+            operatorSymbol = ' + ';
+            break;
+        case '-':
+            operator = subtract;
+            operatorSymbol = ' - ';
+            break;
+        case 'x':
+            operator = multiply;
+            operatorSymbol = ' x ';
+            break;
+        case '/':
+            operator = divide;
+            operatorSymbol = ' / ';
+            break;
+    }
+
+    // num2 will be empty after each operator press.
+    num2 = '';      
+}
+
 operateButtons.forEach((button) => {
     button.addEventListener('click', () => {
-
-        // If input is only '.', operate buttons wont work till
-        // valid input.
-        if (num1 == '.') {
-            return;
-        }
-        dotButton.disabled = false;
-
-        display1.textContent += " " + button.textContent + " ";
-
-        // This check needs to be above the switch statement
-        // because we will operate on previous operator.
-        // Each operator presses will operate on previous numbers
-        // with previous operator.
-
-        // If operators are pressed after "=" newCalc will be false.
-        if (operator && newCalc) {
-            // num1 = operate(operator, Number(num1), Number(num2));
-            display2.textContent = num1;
-            newCalc = false;
-        } else if (operator && !newCalc) {
-            num1 = operate(operator, Number(num1), Number(num2));
-            if (num1 == Infinity || num1 == -Infinity) {
-                display2.textContent = "ERROR";
-            } else {
-                display2.textContent = num1;
-            } 
-        }
-            
-
-        switch (button.textContent) {
-            case '+':
-                operator = add;
-                operatorSymbol = ' + ';
-                break;
-            case '-':
-                operator = subtract;
-                operatorSymbol = ' - ';
-                break;
-            case 'x':
-                operator = multiply;
-                operatorSymbol = ' x ';
-                break;
-            case '/':
-                operator = divide;
-                operatorSymbol = ' / ';
-                break;
-        }
-
-        // num2 will be empty after each operator press.
-        num2 = '';
-        
+        operateButtonFunc(button);
     });
 });
 
-equalButton.addEventListener('click', () => {
+const equalBottonCalculate = () => {
     dotButton.disabled = false;
     // If = is used without entering operator return num1.
     if (!operator) {
@@ -169,7 +170,9 @@ equalButton.addEventListener('click', () => {
     // on results.
     // operator = undefined;
     // num2 = "";
-});
+}
+
+equalButton.addEventListener('click', equalBottonCalculate);
 
 resetButton.addEventListener('click', () => {
     display1.textContent = "";
@@ -182,7 +185,9 @@ resetButton.addEventListener('click', () => {
     dotButton.disabled = false;
 });
 
-delButton.addEventListener('click', () => {
+delButton.addEventListener('click', delButtonFunc);
+
+function delButtonFunc () {
     if (operator) {
         if (num2 != "") {
             display1.textContent = display1.textContent.slice(0, -1);
@@ -195,8 +200,7 @@ delButton.addEventListener('click', () => {
         console.log(num1);
         display1.textContent = num1;
     }
-    
-});
+}
 
 function delDigit(num) {
     let numString = num.toString();
@@ -213,3 +217,32 @@ function delDigit(num) {
         } 
     return num;
 }
+
+// keyboard support
+
+let operatorsObj = {
+    107: operateButtons[0],
+    109: operateButtons[1],
+    106: operateButtons[2],
+    111: operateButtons[3]
+};
+
+let numButtonArray = [""]
+
+window.addEventListener('keydown', (e) => {
+    console.log(e);
+    if (e.keyCode === 13) {
+        // enter key throws tantum, so e.preventDefault is needed.
+        e.preventDefault()
+        equalBottonCalculate();
+    } else if (e.keyCode === 8) {
+        delButtonFunc();
+    } else if (e.keyCode in operatorsObj) {
+        // divide key '/' opens search by default.
+        e.preventDefault();
+        operateButtonFunc(operatorsObj[e.keyCode]);
+    } //else if (e.key in numButtons) {
+    //     console.log(e.key);
+    //     console.log(numButtons[e.key]);
+    // }
+});
